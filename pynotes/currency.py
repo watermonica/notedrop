b71830378cde86a770a0a32d377e0d6eac963231
Ethan Water <waterml@proton.me>
2022-12-05 23:30:30 -0500

    Create currency.py

---
+++ pynotes/currency.py
@@ -0,0 +1,27 @@
+class BankAccountReceivingEnd:
+    ##private
+    __routingnumber = None
+    __accountnumber = None
+
+    def __init__(self, walletName, walletBalance):
+        self.walletName = walletName
+        self.walletBalance = walletBalance
+
+    def displayBalance(self):
+        return(self.walletBalance)
+
+    def sendFunds(self, amount, recievingUser):
+        self.walletBalance -= amount
+        recievingUser.walletBalance += amount
+
+
+a = BankAccountReceivingEnd('a', 100)
+b = BankAccountReceivingEnd('b', 100)
+a.sendFunds(20, b)
+print(str(a.displayBalance()))
+print(str(b.displayBalance()))
+
+
+
+    
+
